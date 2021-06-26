e1c697ee70d979b8082faa4b66247b2f8984dab3
CodingKid124 <84299475+Sebastian-Webster@users.noreply.github.com>
2021-06-27 02:49:54 +1200

    backup to github before moving to working directory

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
 import 'react-native-gesture-handler';
-/*import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';  Uncomment these when react navigation starts working
-*/
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+/*export default function App() {
   return (
-    /*<NavigationContainer>*/
+    <NavigationContainer>
       <View style={styles.body_style}>
         <Text>This is the start of SquareTable's social media app</Text>
         <StatusBar style="auto" />
         <Text style={styles.test_container}>Testing</Text>
       </View>
-    /*</NavigationContainer>*/
+    </NavigationContainer>
     
   );
-}
+}*/
+
+
+const homeScreen = () => {
+    return (
+      <View style={{flex: 1,alignItems: "center", justifyContent: "center"}}>
+      <Text>This is the start of SquareTable's social media app</Text>
+      <StatusBar style="auto" />
+      <Text style={styles.test_container}>Testing</Text>
+    </View>
+    );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="home" component = {homeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
 const styles = StyleSheet.create({
   body_style: {
     flex: 1,
