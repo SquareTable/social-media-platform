f0c15c539ff621ad287f54549f93645231dd81de
CodingKid124 <84299475+Sebastian-Webster@users.noreply.github.com>
2021-06-26 22:14:49 +1200

    add some more text for the app

--- App.js
+++ App.js
@@ -5,17 +5,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is the start of SquareTable's social media app</Text>
       <StatusBar style="auto" />
+      <Text style={styles.test_container}>Testing</Text>
     </View>
+    
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  test_container: {
+    backgroundColor: '#192717',
+    color: "white",
+    fontWeight: "bold",
   },
 });
