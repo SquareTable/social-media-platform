ad3d97afa7da2ef56c9c727c6fca4073f85e6cf3
CodingKid124 <84299475+Sebastian-Webster@users.noreply.github.com>
2021-07-02 11:13:17 +1200

    make small updates

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,28 +11,30 @@ var onPressSettings=() => {
 }
 
 const HomeScreen = ({navigation}) => {
+    /*   LIST OF THINGS TO DO TO THIS PAGE (HomeScreen.js):
+        -Get settings button to work at the top of the screen for iOS and Android
+        -Add a little bit of padding to SafeAreaView for iOS and Android
+    */
     return(
-        <SafeAreaView style={Styles.container}>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#08F1ED', paddingLeft: 10}}>
             <TouchableOpacity onPressIn={() => {onPressSettings();}} activeOpacity={0.5}>
                     <Image
                         source={require('../assets/app_icons/settings.png')} 
-                        style={{ width: 40, height: 40, position: 'absolute', right: 20, top: 10}}
+                        style={{ width: 40, height: 40, position: 'absolute', right: 20, top: 10, zIndex: 9999999999}}
                         resizeMode="contain"
                         resizeMethod="resize"
                     />
             </TouchableOpacity>
-            <View style={{paddingLeft: 15}}>
-                <Text style={{fontSize: 30}}>SocialSquare</Text>
-                <Text>Home Screen</Text>
-                <Image
-                    source={require('../assets/doge.gif')}
-                    resizeMode = 'contain'
-                    style={{
-                        width: 200,
-                        height: 200,
-                    }}
-                />
-            </View>
+            <Text style={{fontSize: 30}}>SocialSquare</Text>
+            <Text>Home Screen</Text>
+            <Image
+                source={require('../assets/doge.gif')}
+                resizeMode = 'contain'
+                style={{
+                    width: 200,
+                    height: 200,
+                }}
+            />
         </SafeAreaView>
     );
 };
@@ -40,9 +42,5 @@ const HomeScreen = ({navigation}) => {
 export default HomeScreen;
 
 const Styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 25,
-        backgroundColor: '#08F1ED',
-    },
+
 });
