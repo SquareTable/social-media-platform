6f6d43b8049230b52b34622f87061e26a6945e74
CodingKid124 <84299475+Sebastian-Webster@users.noreply.github.com>
2021-07-02 11:54:01 +1200

    add scrollview to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, SafeAreaView, ScrollView} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Constants from "expo-constants";
 import styled from "styled-components";
@@ -17,26 +17,28 @@ const HomeScreen = ({navigation}) => {
     */
     return(
         <SafeAreaView style={{flex: 1, backgroundColor: '#08F1ED', paddingLeft: 10}}>
-            <TouchableOpacity onPressIn={() => {onPressSettings();}} activeOpacity={0.5}>
-                    <Image
-                        source={require('../assets/app_icons/settings.png')} 
-                        style={{ width: 40, height: 40, position: 'absolute', right: 20, top: 10, zIndex: 9999999999}}
-                        resizeMode="contain"
-                        resizeMethod="resize"
-                    />
-            </TouchableOpacity>
-            <Text style={{fontSize: 30, textAlign: 'center'}}>SocialSquare</Text>
-            <Text style={{textAlign: 'center'}}>Home Screen</Text>
-            <Image
-                source={require('../assets/doge.gif')}
-                resizeMode = 'contain'
-                style={{
-                    width: 200,
-                    height: 200,
-                    paddingLeft: '50%',
-                    paddingRight: '50%',
-                }}
-            />
+            <ScrollView>
+                <TouchableOpacity onPressIn={() => {onPressSettings();}} activeOpacity={0.5}>
+                        <Image
+                            source={require('../assets/app_icons/settings.png')} 
+                            style={{ width: 40, height: 40, position: 'absolute', right: 20, top: 10, zIndex: 9999999999}}
+                            resizeMode="contain"
+                            resizeMethod="resize"
+                        />
+                </TouchableOpacity>
+                <Text style={{fontSize: 30, textAlign: 'center'}}>SocialSquare</Text>
+                <Text style={{textAlign: 'center'}}>Home Screen</Text>
+                <Image
+                    source={require('../assets/doge.gif')}
+                    resizeMode = 'contain'
+                    style={{
+                        width: 200,
+                        height: 200,
+                        paddingLeft: '50%',
+                        paddingRight: '50%',
+                    }}
+                />
+            </ScrollView>
         </SafeAreaView>
     );
 };
@@ -47,5 +49,8 @@ const Styles = StyleSheet.create({
     centerItems: {
         paddingLeft: '50%',
         paddingRight: '50%',
+    },
+    centerText: {
+        textAlign: 'center'
     }
 });
