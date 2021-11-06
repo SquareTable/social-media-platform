58b94da1bda52a7d4cd8247fba8f78d47a3c2f33
sebtheman <84299475+Sebastian-Webster@users.noreply.github.com>
2021-11-06 22:39:09 +1300

    uninstall react native dynamic search bar as it is unneeded

--- screens/CreateChatScreen.js
+++ screens/CreateChatScreen.js
@@ -7,7 +7,6 @@ import Images from "../posts/images.js";
 import Post from "../posts/post.js";
 import SwitchToggle from "react-native-switch-toggle";
 import {useTheme} from "@react-navigation/native";
-import SearchBar from "react-native-dynamic-search-bar";
 import {
     darkModeStyling, 
     darkModeOn, 
@@ -50,35 +49,24 @@ const CreateChatScreen = ({navigation, route}) => {
     }
     
     return(
-        <KeyboardAvoidingWrapper style={{backgroundColor: colors.primary}}>
-            <BackgroundDarkColor style={{backgroundColor: colors.primary}}>
-                <ChatScreenInformation_Title style={{backgroundColor: colors.primary, borderWidth: 0}}>
-                    <Navigator_BackButton onPressIn={() => {navigation.goBack()}}>
-                        <Image
-                            source={require('../assets/app_icons/back_arrow.png')}
-                            style={{minHeight: 40, minWidth: 40, width: 40, height: 40, maxWidth: 40, maxHeight: 40, borderRadius: 40/2, tintColor: colors.tertiary}}
-                            resizeMode="contain"
-                            resizeMethod="resize"
-                        />
-                    </Navigator_BackButton>
-                    <TestText style={{color: colors.tertiary}}>Create a chat</TestText>
-                </ChatScreenInformation_Title>
-                <SearchBar
-                    darkMode={dark? true : false}
-                    style={{marginVertical: 10}}
-                    placeholder="Search here"
-                    onChangeText={(text) => {console.log(text)}}
-                    onSearchPress={() => console.log("Search Icon is pressed")}
-                    onClearPress={() => console.log("Clear")}
-                    onPress={() => console.log("onPress")}
-                />
-                <SettingsPageItemTouchableOpacity style={{borderColor: colors.borderColor}} onPress={BetaButtonOnPress}>
-                    <SettingsItemImage source={Images.posts.sad_pepe} style={{borderRadius: 1000/2}}/>
-                    <SettingsItemText style={{color: colors.tertiary}}>Toggle Beta Chat Screen</SettingsItemText>
-                </SettingsPageItemTouchableOpacity>
-                <TestText style={{color: colors.tertiary}}>Search functionality coming soon :)</TestText>
-            </BackgroundDarkColor>
-        </KeyboardAvoidingWrapper>
+        <BackgroundDarkColor style={{backgroundColor: colors.primary}}>
+            <ChatScreenInformation_Title style={{backgroundColor: colors.primary, borderWidth: 0}}>
+                <Navigator_BackButton onPressIn={() => {navigation.goBack()}}>
+                    <Image
+                        source={require('../assets/app_icons/back_arrow.png')}
+                        style={{minHeight: 40, minWidth: 40, width: 40, height: 40, maxWidth: 40, maxHeight: 40, borderRadius: 40/2, tintColor: colors.tertiary}}
+                        resizeMode="contain"
+                        resizeMethod="resize"
+                    />
+                </Navigator_BackButton>
+                <TestText style={{color: colors.tertiary}}>Create a chat</TestText>
+            </ChatScreenInformation_Title>
+            <SettingsPageItemTouchableOpacity style={{borderColor: colors.borderColor}} onPress={BetaButtonOnPress}>
+                <SettingsItemImage source={Images.posts.sad_pepe} style={{borderRadius: 1000/2}}/>
+                <SettingsItemText style={{color: colors.tertiary}}>Toggle Beta Chat Screen</SettingsItemText>
+            </SettingsPageItemTouchableOpacity>
+            <TestText style={{color: colors.tertiary}}>Search functionality coming soon :)</TestText>
+        </BackgroundDarkColor>
     );
 };
 
